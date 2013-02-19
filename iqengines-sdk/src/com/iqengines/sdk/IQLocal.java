0e6a5e71ae45f74eb3c202ba706c6ba6b3d15c9d
Michael Soderstrom <mike@local.com>
2013-02-18 16:41:38 -0800

    Null check.

--- iqengines-sdk/src/com/iqengines/sdk/IQLocal.java
+++ iqengines-sdk/src/com/iqengines/sdk/IQLocal.java
@@ -457,7 +457,8 @@ public class IQLocal implements IQLocalApi {
         }
         finally
         {
-        	reader.close();
+        	if (reader != null)
+        	    reader.close();
         }
 	}
     
